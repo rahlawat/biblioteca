381043bb5a1ae7f3c61b595ee62a75e4856e12b3
Renu Ahlawat <renu.ahlawat21@gmail.com>
2012-09-14 16:48:49 +0530

    Test Welcome and Menu

---
+++ src/Biblioteca.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dell
+ * Date: 9/14/12
+ * Time: 3:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Biblioteca {
+}
---
+++ src/UserMenuStub.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dell
+ * Date: 9/14/12
+ * Time: 4:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserMenuStub {
+}
