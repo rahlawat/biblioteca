26a0340f0e56aa010c7d88582b3cf4c880ae9553
Renu Ahlawat <renu.ahlawat21@gmail.com>
2012-09-16 16:49:45 +0530

    Reserved Books and users

---
+++ src/ReservedBook.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dell
+ * Date: 9/16/12
+ * Time: 3:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReservedBook {
+}
---
+++ src/User.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dell
+ * Date: 9/16/12
+ * Time: 1:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+}
