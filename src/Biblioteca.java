779592b766cd8d71ee829c1ddc419cd9d431d8dc
Renu Ahlawat <renu.ahlawat21@gmail.com>
2012-09-16 21:41:50 +0530

    Biblotica

---
+++ src/Biblioteca.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dell
+ * Date: 9/16/12
+ * Time: 7:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Biblioteca {
+}
---
+++ src/ReservedBooks.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dell
+ * Date: 9/16/12
+ * Time: 8:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ReservedBooks {
+}
