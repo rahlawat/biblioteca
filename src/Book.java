717369818b06a9af2ded7f1a0c8867b11f7c704d
Renu Ahlawat <renu.ahlawat21@gmail.com>
2012-09-16 11:19:53 +0530

    Book Details

---
+++ src/Book.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dell
+ * Date: 9/16/12
+ * Time: 10:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Book {
+}
---
+++ test/BookTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dell
+ * Date: 9/16/12
+ * Time: 10:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookTest {
+}
